52be082964afbb7a9438ac6b4fe90cfbf20155e5
Avzl <95623293+Avzl@users.noreply.github.com>
2024-07-23 23:47:19 -0300

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hola papus")
+
+// declare a function called getComputerChoice that randomly returns one of the following string values: “rock”, “paper” or “scissors”
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*100);
+    console.log(randomNumber);
+    if (randomNumber < 33) {
+        return "rock";
+    } else if (randomNumber < 66){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+
+}
+// test if the getComputerChoice returns my expected values
+console.log(getComputerChoice());
